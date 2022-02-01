310be9835e0da4bb776d5a18342c4c98f81f6dbf
Marko Galiman <61890185+mgaliman@users.noreply.github.com>
2022-02-01 20:30:23 +0100

    Update

--- PPPK-Project07/app/src/main/java/hr/algebra/personmanager/dao/PeopleDatabase.kt
+++ PPPK-Project07/app/src/main/java/hr/algebra/personmanager/dao/PeopleDatabase.kt
@@ -1,18 +1,20 @@
 package hr.algebra.personmanager.dao
 
 import android.content.Context
-import androidx.room.Database
-import androidx.room.Room
-import androidx.room.RoomDatabase
-import androidx.room.TypeConverters
+import androidx.room.*
 
-@Database(entities = [Person::class], version = 3)
+@Database(
+    entities = [Person::class], version = 6, exportSchema = true, autoMigrations = [
+        AutoMigration(from = 4, to = 6)]
+)
 @TypeConverters(DateConverter::class)
 abstract class PeopleDatabase : RoomDatabase() {
-    abstract fun personDao() : PersonDao
+
+    abstract fun personDao(): PersonDao
 
     companion object {
-        @Volatile private var INSTANCE: PeopleDatabase? = null
+        @Volatile
+        private var INSTANCE: PeopleDatabase? = null
         fun getInstance(context: Context) =
             INSTANCE ?: synchronized(PeopleDatabase::class.java) {
                 INSTANCE ?: buildDatabase(context).also { INSTANCE = it }
